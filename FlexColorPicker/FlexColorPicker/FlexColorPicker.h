8b3b40f26d6338b1ada86444c0c8ee4d854fd879
Rastislav Mirek <RastislavMirek@users.noreply.github.com>
2018-05-27 17:30:43 +0200

    Added XCode project

---
+++ FlexColorPicker/FlexColorPicker/FlexColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  FlexColorPicker.h
+//  FlexColorPicker
+//
+//  Created by Rastislav Mirek on 27/5/18.
+//  Copyright © 2018 TypeSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexColorPicker.
+FOUNDATION_EXPORT double FlexColorPickerVersionNumber;
+
+//! Project version string for FlexColorPicker.
+FOUNDATION_EXPORT const unsigned char FlexColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexColorPicker/PublicHeader.h>
+
+
